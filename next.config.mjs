2fdb207d7ac37a6275259438016d0ccf805d0fa2
flcianataliaa <flcianataliaa@gmail.com>
2024-08-24 16:54:53 +0700

    add default route

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/overview",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
